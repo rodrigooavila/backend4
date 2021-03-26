f6f6e9e7b97318381467ee32b7f3995fdd603fdf
Rodrigo Avila <rodrigo.magalhaes.a@gmail.com>
2021-03-26 16:49:07 -0300

    Backend 4

--- index.js
+++ index.js
@@ -1,59 +1,7 @@
 const express = require('express'); //invoca express
 const app = express();
 
-/*const middleware = function(req, res, next) {
-    console.log("executando o middleware");
-    next();
-}
 
-app.use(middleware);*/
-
-//let port = 3000;
-//let rotas = require('./rotas');
-
-/*let get_request_time = function(req, res, next) {
-    let tempo_atual = Date.now();
-    let hoje = new Date(tempo_atual);
-    req.request_time = hoje.toUTCString();
-    next();
-}
-
-
-app.use(get_request_time);
-app.get('/', (req, res) => {
-    res.send("Você acessou em " + req.request_time);
-});
-*/
-//app.use('/', rotas); //uso do arquivo de roteamento 
-
-
-/*app.use('/tempo', function(req, res, next) {
-    let tempo_atual = Date.now();
-    let hoje = new Date(tempo_atual);
-    req.request_time = hoje.toUTCString();
-    next();
-})
-
-app.get('/tempo', (req, res) => {
-    res.send("Você acessou em " + req.request_time);
-});
-
-app.get('/', (req, res) => {
-    res.send("Estou no endereço raiz");
-});*/
-
-
-/*app.use(function(req, res, next) {
-    console.log("Início!");
-    next();
-})
-
-app.get('/', (req, res, next) => {
-    res.send("Meio");
-});
-app.get('/', (req, res) => {
-    console.log('Fim');
-});*/
 app.use('/', express.static(__dirname + '/index'));
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/vendor', express.static(__dirname + '/vendor'));
